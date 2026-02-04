299034d3ab8500566cefec20317ba15417586f31
Benworsley121 <Bwo9053@stacmail.net>
2026-02-05 12:34:35 +1300

    1st code basics

--- 1st code.py
+++ 1st code.py
@@ -4,3 +4,36 @@
 #when printing text, it must be in speechmarks.
 print(12345)
 print("hello")
+#different data types:
+#intergers, decimals (floating point numbers)
+#text (string), boolean (true, or false)
+
+# we use variables to store information
+#variables must all be lowercase
+
+name = "Billie"
+
+# if you want multiple variable words in the neame, use underscore
+firt_name = "John"
+Last_name = "smith"
+age = 14
+print(name)
+
+
+#f string
+
+      
+
+
+
+
+#print hello to the usser, and ask their age
+user_name = input("what is your name")
+
+#then print out their name and age
+
+print(user_name)
+
+age = input("what is your age")
+
+print(f"hello {user_name}")

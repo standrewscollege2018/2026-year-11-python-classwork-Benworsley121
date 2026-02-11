54576c7004c630840fc0c37ac5aa20815f9d5776
Benworsley121 <Bwo9053@stacmail.net>
2026-02-12 12:31:17 +1300

    If statements

---
+++ ''' finding requirements for childrens medicine.py
@@ -0,0 +1,23 @@
+''' finding requirements for children medicine'''
+
+#child age limit as a constant
+AGE=12
+age= int(input("how old are you"))
+
+
+
+
+
+if age >= AGE:
+    print("take two 500mg tablets")
+
+    
+
+else: weight=input("how much do you weigh")
+
+
+
+
+dosage = weight * 10
+
+print( f"you should take {dosage} mg of medicine")
